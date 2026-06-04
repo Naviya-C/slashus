37520317b976dc4b8d62684cead0ebaa1361352a
Naveen <naviyac491@gmail.com>
2026-06-04 15:17:11 +0530

    Create Auth Service DataBase Schema

--- backend/services/auth-service/internal/domain/entity/user.go
+++ backend/services/auth-service/internal/domain/entity/user.go
@@ -1,13 +1,19 @@
 package models
 
-import "time"
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
 
 type User struct {
-	ID				string		`gorm:"type:uuid;primaryKey"`
-	FirstName		string		`grom:"size:50;not null"`
-	LastName		string		`grom:"size:50;not null"`
-	Email			string		`grom:"size:100;not null;unique"`
-	PasswordHash	string		`grom:"not null"`
+	ID				uuid.UUID		`gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	FirstName		string			`gorm:"size:50;not null"`
+	LastName		string			`gorm:"size:50;not null"`
+	Email			string			`gorm:"size:100;not null;unique"`
+	PasswordHash	string			`gorm:"not null"`
 	CreatedAt		time.Time	
 	UpdatedAt		time.Time
-}
+
+	RefreshTokens []RefreshToken 	`gorm:"foreignKey:UserID"`
+} 
