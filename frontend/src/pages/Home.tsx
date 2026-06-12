436e50d5c7802ea3ef888bea458c8eb79c273e11
Naveen <naviyac491@gmail.com>
2026-06-13 02:14:35 +0530

    Implementing Home added navbar and hero sections

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Hero from '../components/Hero/Hero';
+import Navbar from '../components/Navbar/Navbar';
+
+
+function Home(){
+    return(
+        <>
+        <Navbar/>
+        <Hero />
+        </>
+    )
+}
+
+export default Home;
