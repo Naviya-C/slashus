5b3fce0cc34a1d90f86a7e668dce9ea135af8b5a
Naveen <naviyac491@gmail.com>
2026-06-13 10:56:56 +0530

    Adding Card to home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import FeatureCard from '../components/Card/FeaturedCard';
 import Hero from '../components/Hero/Hero';
 import Navbar from '../components/Navbar/Navbar';
 
@@ -5,8 +6,13 @@ import Navbar from '../components/Navbar/Navbar';
 function Home(){
     return(
         <>
-        <Navbar/>
-        <Hero />
+            <Navbar/>
+            <Hero />
+            <FeatureCard 
+                title='Summarize'
+                description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
+                image='null'
+            />
         </>
     )
 }
