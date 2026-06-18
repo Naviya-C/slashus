ed00678cd41b6236d10581ee8f991b6e1c7a61fa
Naveen <naviyac491@gmail.com>
2026-06-18 23:24:08 +0530

    Add 3 cards contents out of 4

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -16,19 +16,19 @@ function Home(){
                 reverse = {false}
             />
             <FeatureCard 
-                title='Summarize'
-                description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
+                title='Quizzers'
+                description='Create intelligent, interactive quizzes from your resources in seconds.Evaluate understanding, reinforce key concepts, and support effective learning through personalized assessments.'
                 image='null'
                 reverse = {true}
             />
             <FeatureCard 
-                title='Summarize'
-                description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
+                title='Papers'
+                description='Create professional question papers, marking schemes, and automated grading systems in seconds.Simplify assessment creation and evaluation while maintaining quality and consistency.'
                 image='null'
                 reverse = {false}
             />
             <FeatureCard 
-                title='Summarize'
+                title='Auto Marking'
                 description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
                 image='null'
                 reverse = {true}
