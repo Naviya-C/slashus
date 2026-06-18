7858a1e593116b451bc40a5926bef8462b48636c
Naveen <naviyac491@gmail.com>
2026-06-18 23:08:19 +0530

    Adding cards in home page and footer

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import FeatureCard from '../components/Card/FeaturedCard';
 import Hero from '../components/Hero/Hero';
+import Footer from '../components/Footer/footer';
 import Navbar from '../components/Navbar/Navbar';
 
 
@@ -12,7 +13,27 @@ function Home(){
                 title='Summarize'
                 description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
                 image='null'
+                reverse = {false}
             />
+            <FeatureCard 
+                title='Summarize'
+                description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
+                image='null'
+                reverse = {true}
+            />
+            <FeatureCard 
+                title='Summarize'
+                description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
+                image='null'
+                reverse = {false}
+            />
+            <FeatureCard 
+                title='Summarize'
+                description='Intelligently summarize your educational resources into clear and concise insights.Extract key concepts, important information, and essential takeaways with ease.Save time while improving comprehension and knowledge retention.Transform complex content into accessible and actionable learning materials.'
+                image='null'
+                reverse = {true}
+            />
+            <Footer />
         </>
     )
 }
