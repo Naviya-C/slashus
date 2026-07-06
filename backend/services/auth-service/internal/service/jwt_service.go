832483119368ebfebb6077ad2d6eb5d3439f0abc
Naveen <naviyac491@gmail.com>
2026-07-06 10:13:55 +0530

    adding generate refresh token logic

--- backend/services/auth-service/internal/service/jwt_service.go
+++ backend/services/auth-service/internal/service/jwt_service.go
@@ -2,12 +2,16 @@
 package service
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"time"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
 type JWTService interface {
-	GenerateToken(userID string, email string) (string, error)
+	GenerateTokenAccess(userID string, email string) (string, error)
+	GenerateRefreshToken() (string, error)
 }
 
 type jwtService struct {
@@ -18,11 +22,11 @@ type jwtService struct {
 func NewJWTService(secret string) JWTService {
 	return &jwtService{
 		secretKey:      []byte(secret),
-		expireDuration: time.Minute * 15, // Token expires in 15 hours
+		expireDuration: time.Minute * 15, // Token expires in 15 minutes
 	}
 }
 
-func (s *jwtService) GenerateToken(userID string, email string) (string, error) {
+func (s *jwtService) GenerateTokenAccess(userID string, email string) (string, error) {
 	// Create the standard and custom claims
 	claims := jwt.MapClaims{
 		"sub":   userID,                                 // Subject (User ID)
@@ -36,4 +40,13 @@ func (s *jwtService) GenerateToken(userID string, email string) (string, error) 
 
 	// Sign the token with your secret key
 	return token.SignedString(s.secretKey)
+}
+
+func (s *jwtService) GenerateRefreshToken() (string, error){
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+        return "", err
+    }
+
+	return hex.EncodeToString(b), nil
 }
