76e7473b92a44bb01695f7bc4dba09e5d1c56a54
Naveen <naviyac491@gmail.com>
2026-07-06 10:13:02 +0530

    Adding logic to store, delete, find by refresh token from database

--- backend/services/auth-service/internal/infrastructure/repository/postgres_user_repository.go
+++ backend/services/auth-service/internal/infrastructure/repository/postgres_user_repository.go
@@ -40,3 +40,33 @@ func (r *PostgresUserRepository) FindByEmail(email string) (*models.User, error)
 
     return &user, nil
 }
+
+func (r *PostgresUserRepository) SaveRefreshToken(
+	token *models.RefreshToken) error {
+	return r.db.Create(token).Error
+}
+
+func (r *PostgresUserRepository) DeleteRefreshToken(
+	tokenStr string) error {
+	return r.db.Where("token_hash = ?", tokenStr).Delete(&models.RefreshToken{}).Error
+}
+
+func (r *PostgresUserRepository) FindUserByRefreshToken(tokenStr string) (*models.User, *models.RefreshToken, error) {
+	var tokenRecord models.RefreshToken
+	
+	// Find the token row first
+	if err := r.db.Where("token_hash = ?", tokenStr).First(&tokenRecord).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil, nil // Token not found cleanly
+		}
+		return nil, nil, err
+	}
+
+	// Use the UserID foreign key to get the corresponding User details
+	var user models.User
+	if err := r.db.First(&user, "id = ?", tokenRecord.UserID).Error; err != nil {
+		return nil, nil, err
+	}
+
+	return &user, &tokenRecord, nil
+}
