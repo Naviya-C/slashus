f73ded634075123c160557b44c6af739dbf9821a
Naveen <naviyac491@gmail.com>
2026-07-23 15:03:51 +0530

    Add hook for navigation navbar and login signup pages

---
+++ frontend/src/Hooks/useNav.ts
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+export function useNav() {
+    const navigate = useNavigate();
+
+    return {
+        goToLogin: () => navigate("/login"),
+        goToRegister: () => navigate("/register"),
+        goToHome: () => navigate("/"),
+    };
+}
